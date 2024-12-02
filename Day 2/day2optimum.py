4be2404619d2e5376f759816b27f4adfa8e33ccc
Dhaval Pandey <dhavalpandey@Dhavals-MacBook-Air.local>
2024-12-02 11:32:59 +0000

    experiment with optimised code

---
+++ Day 2/day2optimum.py
@@ -0,0 +1,74 @@
+import sys
+
+def check_increment_order(arr): 
+    rule_followed = False   
+    increasing = False
+    if arr[1] - arr[0] > 0:
+        increasing = True
+    if increasing:
+        temp = sorted(arr)
+    else:
+        temp = sorted(arr, reverse=True)
+    if arr == temp:
+        rule_followed = True
+    
+    return rule_followed
+    
+def check_is_safe(arr):
+    is_safe = True
+    increasing = False
+    if arr[1] - arr[0] > 0:
+        increasing = True
+    for i in range(len(arr)-1):
+        curr = arr[i]
+        next = arr[i+1]
+        diff = abs(next - curr)
+        if increasing:
+            if next - curr < 0:
+                is_safe = False
+                break
+        else:
+            if next - curr > 0:
+                is_safe = False
+                break
+        if diff > 3 or diff == 0:
+            is_safe = False
+            break
+
+    return is_safe
+
+def find_bad_number(arr):
+    for i in range(len(arr)-1):
+        temp = [arr[i], arr[i+1]]
+        if not check_is_safe(temp) and check_increment_order(arr):
+            return temp
+
+def main(file):
+    safe_count = 0
+    with open(file) as f:
+        lines = f.readlines()
+    for line in lines:
+        text = line.strip().split(" ")
+        arr = [int(str) for str in text]
+        is_safe = check_is_safe(arr) and check_increment_order(arr)
+
+        if is_safe:
+            safe_count += 1
+        else:
+            print(arr)
+            to_pop = find_bad_number(arr)
+            print(to_pop)
+    
+    print(safe_count)
+
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    inputer_file = ""
+    if mode == "test":
+        input_file = "test.txt"
+    elif mode == "input" or mode == "i":
+        input_file = "input.txt"
+    else:
+        input_file = "test.txt"
+    
+    main(input_file)
