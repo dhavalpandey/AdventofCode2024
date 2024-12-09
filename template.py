f65cba8caff720392f981b59962a35ade72b8058
Dhaval Pandey <dhavalpandey@Dhavals-MacBook-Air.local>
2024-12-09 22:35:16 +0000

    copy functionality to template

--- template.py
+++ template.py
@@ -1,12 +1,19 @@
+import sys
+import pyperclip as pc
+sys.setrecursionlimit(10**6)
+infinity = float("inf")
+
 def main(file):
     with open(file) as f:
         lines = f.readlines()
     for line in lines:
         text = line.strip()
+        return None
 
 
 if __name__ == "__main__":
-    import sys
     mode = sys.argv[1] if len(sys.argv) > 1 else "test"
     input_file = "test.txt" if mode == "test" else "input.txt"
-    main(input_file)
+    ans = main(input_file)
+    print(ans)
+    pc.copy(ans)
